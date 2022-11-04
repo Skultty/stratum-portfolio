2b624a7905a91618dfece07e56fc527d544af095
Samuel <samuel1skultety@gmail.com>
2022-11-04 01:37:55 +0100

    fix: deleted unnecessary files and changed icon

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ import Portfolio from "./components/Portfolio";
 
 
 function App() {
-  let arr = [];
-  for (let i = 0; i < 2000; i++) {
-    arr.push("<p>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.</p>");
-  };
-  console.log(arr);
   return (
     <main>
       <Navbar/>
